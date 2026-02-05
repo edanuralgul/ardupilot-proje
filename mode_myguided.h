51b376ac5891493e828b080f39003791334e81b8
edanuralgul <edanuralgul3@gmail.com>
2026-02-05 13:08:10 +0300

    Update mode_myguided.h

--- mode_myguided.h
+++ mode_myguided.h
@@ -2,14 +2,13 @@
 
 #include "mode_guided.h"
 
-// Guided'dan türetilen özel mode
 class ModeMyGuided : public ModeGuided
 {
 public:
-    bool init(bool ignore_checks) override; // mode başlangıcı
-    void run() override;                    // ana döngü fonksiyonu
+    bool init(bool ignore_checks) override;
+    void run() override;
 
 private:
-    int step = 0;     // davranış adımı
-    float timer = 0;  // zaman sayacı
+    int adim = 0;
+    float zamanlayici = 0;
 };
